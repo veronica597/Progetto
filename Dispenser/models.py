dc446ea1a8cb7ed4a216db4f28e9e71abed9b139
veronicapecchioli <veronica.pecchioli@outlook.it>
2019-10-04 12:34:00 +0200

    aggiunta colonna per uso utente

--- Dispenser/models.py
+++ Dispenser/models.py
@@ -8,6 +8,7 @@ class DatiRaccolti(models.Model):
 
     date = models.DateTimeField(auto_now=True)
     erogation= models.BooleanField(default=False)
+    userMod= models.BooleanField(default=False)
 
 def __str__(self):
     return self.language.name
