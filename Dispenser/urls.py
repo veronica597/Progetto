4bc0def6e02316dfa763346339b0a81496677013
Chiara Albisani <chiaraalbi46@gmail.com>
2019-11-26 00:18:25 +0100

    Aggiunte view periodo e grafico_periodo per gestire la renderizzazione di un grafico che raccolga i dati desiderati sul periodo (mese/settimana). A questo proposito e' stata aggiunto il file periodo.html per gestire la logica richiesta/risposta lato html. Sono state effettuate modifiche ai file chartInside.html e get_post.html per distinguere i casi di grafico/tabella per un giorno qualunque e grafico/tabella per il giorno corrente. In particolare in quest'ultimo e' stata inserita una parte dedicata all'aggiornamento continuo dei dati

--- Dispenser/urls.py
+++ Dispenser/urls.py
@@ -6,18 +6,18 @@ from django.views.generic import TemplateView
 urlpatterns = [
     # url(r'^$', views.index, name='index'),
     # url(r'', include('Dispenser.urls')),
-     url(r'^$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
 
     url(r'^sensor/', views.sensor, name='sensor'),
     # url(r'^admin/', admin.site.urls),
-    url(r'^client/', views.client, name='client'),
+    url(r'^client/', views.client, name='client'),  # get_post.html
 
     # url(r'^ajax/', views.ajax, name='ajax'),
     # url(r'^visual/', views.visual, name='visual'),
 
     url(r'^gajax/', views.get_ajax, name='gajax'),
 
-    #url(r'^$', views.viewData, name='viewData'),  # /dispenser --> dati tabella. VIEW CHIARA
+    # url(r'^$', views.viewData, name='viewData'),  # /dispenser --> dati tabella. VIEW CHIARA
 
     # url(r'^chart/', views.chartData, name='chart'),
     url(r'^aggiornamento/', views.LastDate, name='agg'),
@@ -26,6 +26,9 @@ urlpatterns = [
     url(r'^dati/', views.sendData, name='dati'),
 
     url(r'^giorno/', views.ultimoDato, name='giorno'),
+    url(r'^periodo/', views.periodo, name='periodo'),
+    url(r'^grafPeriodo/', views.grafico_periodo, name='grafP'),
+
 
 
 
--- Dispenser/views.py
+++ Dispenser/views.py
@@ -14,8 +14,9 @@ from django.db import models
 from django.db.models import Count, Q
 
 
+@csrf_exempt
 def index(request):
-    #return HttpResponse("<p> Food Dispenser Application </p>")
+    # return HttpResponse("<p> Food Dispenser Application </p>")
     return render(request, 'home.html')
 
 
@@ -74,7 +75,13 @@ def client(request):  # processa i dati inviati a seguito del click dell'utente
 
     if request.method == 'GET':
         print('get')
-        return render(request, 'get_post.html')
+        oggi = datetime.date.today()
+        print(oggi)
+        context = {
+            'righe': DatiRaccolti.objects.values().filter(date=oggi),
+        }
+
+        return render(request, 'get_post.html', context)
 
     return HttpResponseForbidden
 
@@ -245,76 +252,136 @@ def chartData(request):  # view che renderizza il CHART "statico"
 
 
 @csrf_exempt
-def grafico(request):
-    context = {'righe': DatiRaccolti.objects.all().filter(date__gte="2019-11-05")}  # dati del giorno corrente
+def grafico(request):  # verra' richiamata da una funzione ajax --> inserire la parte per interpretare request.GET
+    context = {'righe': DatiRaccolti.objects.values().filter(date__gte="2019-10-22")}  # dati del giorno corrente
     return render(request, 'chartInside.html', context)
 
 
 @csrf_exempt
 def sendData(request):  # view che invia i dati per costruire il CHART lato html
-    # print(request.body)
-    # print(request.GET)
+
     anno = request.GET.__getitem__('anno')
     mese = request.GET.__getitem__('mese')
     giorno = request.GET.__getitem__('giorno')
     stringa = anno + "-" + mese + "-" + giorno
-    # print(stringa)
+    print(stringa)
 
-    dataset = DatiRaccolti.objects \
-        .values('date') \
-        .filter(date__gte=stringa) \
-        .annotate(erogatedA=Count('date', filter=Q(erogation=True, userMod=False)),
-                  erogatedU=Count('date', filter=Q(erogation=True, userMod=True))) \
-        .order_by('date')
+    # dataset = DatiRaccolti.objects \
+    #     .values('date') \
+    #     .filter(date__gte=stringa) \
+    #     .annotate(erogatedA=Count('date', filter=Q(erogation=True, userMod=False)),
+    #               erogatedU=Count('date', filter=Q(erogation=True, userMod=True))) \
+    #     .order_by('date')
 
-    categories = list()
-    erogAutomatic = list()
-    erogUser = list()
-    dateStringhe = list()
+    eA = DatiRaccolti.objects.values('date').filter(date__gte=stringa, erogation=True, userMod=False).order_by('date').count()
+    eU = DatiRaccolti.objects.values('date').filter(date__gte=stringa, erogation=True, userMod=True).order_by('date').count()
 
-    for entry in dataset:
-        categories.append('%s Class' % entry['date'])  #
-        # print(entry['date'])
-        # print(entry['date'].strftime("%d/%m/%y %H:%M:%S:%f"))
-        dateStringhe.append(entry['date'].strftime("%d/%m/%y %H:%M:%S"))  # :%f per i millisecondi
-        erogAutomatic.append(entry['erogatedA'])
-        erogUser.append(entry['erogatedU'])
+    # dovro' mettere tipo date__day = giorno, date__month = mese , ...
 
-    # print(categories)
-    # print(dateStringhe)
-    cat = json.dumps(categories)
-    # print(cat)
-    autoE = json.dumps(erogAutomatic)
-    userE = json.dumps(erogUser)
+    print("eA: " + str(eA))
+    print("eU: " + str(eU))
 
-    macro = list()  # incapsulo gli ultimi dati in una lista
-    macro.append(cat)
-    macro.append(autoE)
-    macro.append(userE)
-    # print(macro)
-    # print(json.dumps(macro))
+    # categories = list()
+    # erogAutomatic = list()
+    # erogUser = list()
+    # dateStringhe = list()
+    #
+    # for entry in dataset:
+    #     categories.append('%s Class' % entry['date'])
+    #     # print(entry['date'])
+    #     # print(entry['date'].strftime("%d/%m/%y %H:%M:%S:%f"))
+    #     dateStringhe.append(entry['date'].strftime("%d/%m/%y %H:%M:%S"))  # :%f per i millisecondi
+    #     erogAutomatic.append(entry['erogatedA'])
+    #     erogUser.append(entry['erogatedU'])
+    #
+    # # print(categories)
+    # # print(dateStringhe)
+    # cat = json.dumps(categories)
+    # # print(cat)
+    # autoE = json.dumps(erogAutomatic)
+    # userE = json.dumps(erogUser)
+    #
+    # macro = list()  # incapsulo gli ultimi dati in una lista
+    # macro.append(cat)
+    # macro.append(autoE)
+    # macro.append(userE)
 
-    return JsonResponse(dateStringhe, safe=False)
+    erogazioni = [stringa, eA, eU]
+    return JsonResponse(erogazioni, safe=False)
+
+    # return JsonResponse(macro, safe=False)  # dateStringhe
+
+    # context = {'giorno': stringa, 'erogA': eA, 'erogU': eU}
+    # return render(request, 'chartInside.html', context)
 
 
 @csrf_exempt
 def ultimoDato(request):  # per aggiornamento tabella e chart
-    # anno = request.GET.__getitem__('anno')
-    # mese = request.GET.__getitem__('mese')
-    # giorno = request.GET.__getitem__('giorno')
-    # stringa = anno + "-" + mese + "-" + giorno
-    elementi = DatiRaccolti.objects.values().filter(date__gte='2019-11-05')  # stringa
+    anno = request.GET.__getitem__('anno')
+    mese = request.GET.__getitem__('mese')
+    giorno = request.GET.__getitem__('giorno')
+    stringa = anno + "-" + mese + "-" + giorno
+    print(stringa)
+    elementi = DatiRaccolti.objects.values().filter(date__gte=stringa)
 
     # .strftime("%b. %d, %Y, %H:%M %p"))  # :%f per i millisecondi
 
     lu = len(elementi)
-    # print(lu)
-    # f = elementi[lu] # invio sempre l'ultimo elemento
-    f = elementi[0]
+    print(lu)
+    if lu == 0:  # il database non contiene dati per il giorno corrente
+        f = "Non ci sono erogazioni per il giorno corrente"
+
+    else:
+        f = elementi[lu - 1]  # invio sempre l'ultimo elemento
 
     print(f)
 
     return JsonResponse(f, safe=False)
+
+
+def periodo(request):  # per filtraggio mese/settimana
+    annoI = request.GET.__getitem__('annoI')
+    meseI = request.GET.__getitem__('meseI')
+    giornoI = request.GET.__getitem__('giornoI')
+
+    stringaI = annoI + "-" + meseI + "-" + giornoI
+
+    annoF = request.GET.__getitem__('annoF')
+    meseF = request.GET.__getitem__('meseF')
+    giornoF = request.GET.__getitem__('giornoF')
+
+    stringaF = annoF + "-" + meseF + "-" + giornoF
+
+    # per grafico stile giorno singolo --> ho due colonne una per le erogazioni automatiche e una per le erogazioni utente
+
+    eA = DatiRaccolti.objects.values('date').filter(date__gte=stringaI, date__lte=stringaF, erogation=True, userMod=False).order_by('date').count()
+    # primo compreso, ultimo escluso --mi va bene ?
+    # anche utilizzando date__range=[..] ottengo lo stesso risultato
+    eU = DatiRaccolti.objects.values('date').filter(date__gte=stringaI, date__lte=stringaF, erogation=True, userMod=True).order_by('date').count()
+
+    print("eA: " + str(eA))
+    print("eU: " + str(eU))
+
+    erogazioni = [eA, eU]  # come categories cosa passo ??
+    return JsonResponse(erogazioni, safe=False)
+
+    # return JsonResponse(macro, safe=False)  # dateStringhe
+
+    # context = {'giorno': stringa, 'erogA': eA, 'erogU': eU}
+    # return render(request, 'chartInside.html', context)
+
+    # se voglio un grafico in cui sull'asse x ho tutti i giorni del periodo devo poter sapere quali sono questi giorni e contare per ognuno di loro i dati come
+    # se fossero un giorno singolo
+
+    # datePeriodo = DatiRaccolti.objects.values('date').distinct().filter(date__range=[stringaI, stringaF])  # primo compreso, ultimo escluso
+    # print(datePeriodo)
+
+    # return HttpResponse("quante date ci sono nel periodo ?")
+
+
+def grafico_periodo(request):
+    # mettere context
+    return render(request, 'periodo.html')
 
 
 
