6d016db484e9dd6143b903ed920a6103393a1f62
Chiara Albisani <chiaraalbi46@gmail.com>
2019-11-26 16:06:21 +0100

    Aggiunti tasti per grafici erogazioni/passaggi, erogazioni automatiche/utente, erogazioni giorno/notte nel file chartInside.html, e relative views. Eliminati file inessenziali.

--- Dispenser/urls.py
+++ Dispenser/urls.py
@@ -1,37 +1,24 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from Dispenser import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    # url(r'^$', views.index, name='index'),
-    # url(r'', include('Dispenser.urls')),
-    url(r'^$', views.home, name='home'),
 
     url(r'^sensor/', views.sensor, name='sensor'),
-    # url(r'^admin/', admin.site.urls),
+
     url(r'^client/', views.client, name='client'),  # get_post.html
 
-    # url(r'^ajax/', views.ajax, name='ajax'),
-    # url(r'^visual/', views.visual, name='visual'),
+    url(r'^chart/', views.grafico, name='grafico'),  # giorno qualunque NO giorno corrente
+    url(r'^dati/', views.sendData, name='dati'),  # per ricevere i dati di erogazioni automatiche/utente
 
-    url(r'^gajax/', views.get_ajax, name='gajax'),
+    url(r'^giorno/', views.ultimoDato, name='giorno'),  # per avere l'ultima riga -- aggiornamento
 
-    # url(r'^$', views.viewData, name='viewData'),  # /dispenser --> dati tabella. VIEW CHIARA
-
-    # url(r'^chart/', views.chartData, name='chart'),
-    url(r'^aggiornamento/', views.LastDate, name='agg'),
-
-    url(r'^chart/', views.grafico, name='grafico'),
-    url(r'^dati/', views.sendData, name='dati'),
-
-    url(r'^giorno/', views.ultimoDato, name='giorno'),
     url(r'^periodo/', views.periodo, name='periodo'),
     url(r'^grafPeriodo/', views.grafico_periodo, name='grafP'),
 
+    url(r'^erog/', views.invioErog, name='erog'),  # per ricevere i dati di erogazioni/passaggi
 
-
-
+    url(r'^erogGN/', views.invioGiornoNotte, name='erog'),  # per ricevere i dati di erogazioni giorno/notte
 
 
 ]
--- Dispenser/views.py
+++ Dispenser/views.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseForbidden, Http404
 from .models import DatiRaccolti
 from django.views.decorators.csrf import csrf_exempt
 import json
 import datetime
-from django.core import serializers
 from django.http import JsonResponse
 from django.utils import timezone
 from django.db import models
-from django.db.models import Count, Q
 
 
 @csrf_exempt
@@ -22,14 +20,6 @@ def index(request):
 
 def profile(request):
     return HttpResponse("<p>Profile page of user </p>")
-
-
-@csrf_exempt
-def home(request):  # renderizza i dati del database
-    if request.method == 'GET':
-        return render(request, 'index.html', {
-            'righe': DatiRaccolti.objects.all(),
-        })
 
 
 @csrf_exempt
@@ -57,7 +47,7 @@ def sensor(request):  # processa i dati da inserire nel database
 @csrf_exempt
 def client(request):  # processa i dati inviati a seguito del click dell'utente
 
-    if request.method == 'POST':
+    if request.method == 'POST':  # RIGUARDARE
         print('post')
         print(request.body)
         body_unicode = request.body.decode('utf-8')
@@ -85,193 +75,22 @@ def client(request):  # processa i dati inviati a seguito del click dell'utente
 
     return HttpResponseForbidden
 
-#
-# @csrf_exempt
-# def ajax(request):
-#     return render(request, 'ajax_test.html', )
-#
-#
-# @csrf_exempt
-# def visual(request):
-#     increment = int(request.GET['append_increment'])
-#     increment_to = increment + 10
-#     return render(request, 'render_ajax.html', {
-#         'righe': DatiRaccolti.objects.filter(timeMod=1)[increment:increment_to],
-#     })
-
 
 @csrf_exempt
-def get_ajax(request):  # view che chiamo per avere l'ultimo dato postato per aggiornare la TABELLA
-
-    def myConverter(o):
-        if isinstance(o, datetime.datetime):
-            return o.__str__()
-
-    if request.is_ajax() or request.method == "GET":
-
-        elementi = DatiRaccolti.objects.values().filter(date__gte="2019-11-05")
-        # for el in elementi:
-        #     elem.append(json.dumps(el, default=myConverter))
-        #
-        # print(elem)
-        # dictionary = list(elementi)
-        #print(dictionary)
-
-        # id = len(elementi)
-        # data = json.dumps(elementi)
-        #data = serializers.serialize('json', elementi)
-        #print(data)
-        # element_id = json.dumps(id)
-        # items = ["Mario", "Lorenzo", "Pino"]
-        # data = json.dumps(dictionary, default=myConverter)
-
-        # print(data)
-        # print(elementi)
-        # return HttpResponse(data, content_type="application/json")
-        # x = DatiRaccolti.objects.values().filter(date__lte=timezone.datetime(2019, 10, 16, 13, 5, 27, 892171))
-
-        lu = len(elementi)
-        print(lu)
-        f = elementi[0]  # lu - 1
-        print(f)
-
-        # print(data)
-        return JsonResponse(f, safe=False)
-        # return JsonResponse({'righe': list(elementi)})
-        # return HttpResponse({'righe': elementi})
-        # return render(request, "get_ajax.html", {'righe': elementi})
-    else:
-        raise Http404
-
-
-@csrf_exempt
-def viewData(request):  # view pagina utente -- dati giorno corrente -- TABELLA
-    if request.method == 'GET':
-        context = {'righe': DatiRaccolti.objects.values().filter(date__gte="2019-11-05")}  # dati del giorno corrente
-        return render(request, "get_ajax.html", context)
-
-
-@csrf_exempt
-def LastDate(request):  # view che chiamo per avere i dati per aggiornare il CHART
-    dataset = DatiRaccolti.objects \
-        .values('date') \
-        .filter(date__gte="2019-11-05") \
-        .annotate(erogatedA=Count('date', filter=Q(erogation=True, userMod=False)),
-                  erogatedU=Count('date', filter=Q(erogation=True, userMod=True))) \
-        .order_by('date')
-
-    categories = list()
-    erogAutomatic = list()
-    erogUser = list()
-
-    for entry in dataset:
-        categories.append('%s Class' % entry['date'])
-        erogAutomatic.append(entry['erogatedA'])
-        erogUser.append(entry['erogatedU'])
-
-    # old_cat = categories
-    # old_erogA = erogAutomatic
-    # old_erogU = erogUser
-
-    lastCat = categories[len(categories) - 1]  # l'ultima data
-    print(lastCat)
-    lastErogA = erogAutomatic[len(erogAutomatic) - 1]  # l'ultimo valore di erogazione automatica
-    lastErogU = erogUser[len(erogUser) - 1]  # l'ultimo valore di erogazione utente
-
-    # automatic_series = {
-    #     'name': 'Erogazioni automatiche',
-    #     'data': lastErogA,  # [old_erogA, lastErogA] vedere
-    #     'color': 'green'
-    # }
-    #
-    # user_series = {
-    #     'name': 'Erogazioni utente',
-    #     'data': lastErogU,  # [old_erogU, lastErogU] vedere
-    #     'color': 'red'
-    # }
-    #
-    # chart = {
-    #     'chart': {'type': 'column'},
-    #     'title': {'text': 'Romano Ts'},
-    #     'xAxis': {'categories': lastCat},  # [old_cat, lastCat] vedere
-    #     'series': [automatic_series, user_series]
-    # }
-
-    nicolino = json.dumps(lastCat)
-    nicol = json.dumps(lastErogU)
-    locin = json.dumps(lastErogA)
-
-    macro = list()  # incapsulo gli ultimi dati in una lista
-    macro.append(nicolino)
-    macro.append(nicol)
-    macro.append(locin)
-
-    return JsonResponse(macro, safe=False)  # chart se passo tutto il chart modificato
-
-
-@csrf_exempt
-def chartData(request):  # view che renderizza il CHART "statico"
-    dataset = DatiRaccolti.objects \
-        .values('date') \
-        .filter(date__gte="2019-11-05") \
-        .annotate(erogatedA=Count('date', filter=Q(erogation=True, userMod=False)),
-                  erogatedU=Count('date', filter=Q(erogation=True, userMod=True))) \
-        .order_by('date')
-
-    categories = list()
-    erogAutomatic = list()
-    erogUser = list()
-
-    for entry in dataset:
-        categories.append('%s Class' % entry['date'])
-        erogAutomatic.append(entry['erogatedA'])
-        erogUser.append(entry['erogatedU'])
-
-    automatic_series = {
-        'name': 'Erogazioni automatiche',
-        'data': erogAutomatic,
-        'color': 'green'
-    }
-
-    user_series = {
-        'name': 'Erogazioni utente',
-        'data': erogUser,
-        'color': 'red'
-    }
-
-    chart = {
-        'chart': {'type': 'column'},
-        'title': {'text': 'Romano Ts'},
-        'xAxis': {'categories': categories},
-        'series': [automatic_series, user_series]
-    }
-
-    dump = json.dumps(chart)
-
-    return render(request, 'chart.html', {'chart': dump})
-
-
-@csrf_exempt
-def grafico(request):  # verra' richiamata da una funzione ajax --> inserire la parte per interpretare request.GET
+def grafico(request):  # inserire la parte per interpretare request.GET
+    # quando clicco invio dei parametri che qui vengono recuperati per fitrare i dati-->  invio il context
     context = {'righe': DatiRaccolti.objects.values().filter(date__gte="2019-10-22")}  # dati del giorno corrente
     return render(request, 'chartInside.html', context)
 
 
 @csrf_exempt
-def sendData(request):  # view che invia i dati per costruire il CHART lato html
+def sendData(request):  # view che invia i dati per costruire il CHART erogazioni utente/automatiche lato html
 
     anno = request.GET.__getitem__('anno')
     mese = request.GET.__getitem__('mese')
     giorno = request.GET.__getitem__('giorno')
     stringa = anno + "-" + mese + "-" + giorno
     print(stringa)
-
-    # dataset = DatiRaccolti.objects \
-    #     .values('date') \
-    #     .filter(date__gte=stringa) \
-    #     .annotate(erogatedA=Count('date', filter=Q(erogation=True, userMod=False)),
-    #               erogatedU=Count('date', filter=Q(erogation=True, userMod=True))) \
-    #     .order_by('date')
 
     eA = DatiRaccolti.objects.values('date').filter(date__gte=stringa, erogation=True, userMod=False).order_by('date').count()
     eU = DatiRaccolti.objects.values('date').filter(date__gte=stringa, erogation=True, userMod=True).order_by('date').count()
@@ -281,35 +100,8 @@ def sendData(request):  # view che invia i dati per costruire il CHART lato html
     print("eA: " + str(eA))
     print("eU: " + str(eU))
 
-    # categories = list()
-    # erogAutomatic = list()
-    # erogUser = list()
-    # dateStringhe = list()
-    #
-    # for entry in dataset:
-    #     categories.append('%s Class' % entry['date'])
-    #     # print(entry['date'])
-    #     # print(entry['date'].strftime("%d/%m/%y %H:%M:%S:%f"))
-    #     dateStringhe.append(entry['date'].strftime("%d/%m/%y %H:%M:%S"))  # :%f per i millisecondi
-    #     erogAutomatic.append(entry['erogatedA'])
-    #     erogUser.append(entry['erogatedU'])
-    #
-    # # print(categories)
-    # # print(dateStringhe)
-    # cat = json.dumps(categories)
-    # # print(cat)
-    # autoE = json.dumps(erogAutomatic)
-    # userE = json.dumps(erogUser)
-    #
-    # macro = list()  # incapsulo gli ultimi dati in una lista
-    # macro.append(cat)
-    # macro.append(autoE)
-    # macro.append(userE)
-
     erogazioni = [stringa, eA, eU]
     return JsonResponse(erogazioni, safe=False)
-
-    # return JsonResponse(macro, safe=False)  # dateStringhe
 
     # context = {'giorno': stringa, 'erogA': eA, 'erogU': eU}
     # return render(request, 'chartInside.html', context)
@@ -339,6 +131,7 @@ def ultimoDato(request):  # per aggiornamento tabella e chart
     return JsonResponse(f, safe=False)
 
 
+@csrf_exempt
 def periodo(request):  # per filtraggio mese/settimana
     annoI = request.GET.__getitem__('annoI')
     meseI = request.GET.__getitem__('meseI')
@@ -365,23 +158,62 @@ def periodo(request):  # per filtraggio mese/settimana
     erogazioni = [eA, eU]  # come categories cosa passo ??
     return JsonResponse(erogazioni, safe=False)
 
-    # return JsonResponse(macro, safe=False)  # dateStringhe
+    # context = {'giorno': stringa, 'erogA': eA, 'erogU': eU}
+    # return render(request, 'chartInside.html', context)
+
+
+@csrf_exempt
+def grafico_periodo(request):
+
+    # mettere context
+    # context = {'righe': DatiRaccolti.objects.values('date').filter(date__gte=stringaI, date__lte=stringaF, erogation=True, userMod=False).order_by('date')}
+    return render(request, 'periodo.html')
+
+
+@csrf_exempt
+def invioErog(request):  # views per grafico erogazioni/passaggi
+    anno = request.GET.__getitem__('anno')
+    mese = request.GET.__getitem__('mese')
+    giorno = request.GET.__getitem__('giorno')
+    stringa = anno + "-" + mese + "-" + giorno
+    print(stringa)
+
+    e = DatiRaccolti.objects.values('date').filter(date__gte=stringa, erogation=True).order_by('date').count()
+    noE = DatiRaccolti.objects.values('date').filter(date__gte=stringa, erogation=False).order_by('date').count()
+
+    # dovro' mettere tipo date__day = giorno, date__month = mese , ...
+
+    print("e: " + str(e))
+    print("noE: " + str(noE))
+
+    erogazioni = [stringa, e, noE]
+    return JsonResponse(erogazioni, safe=False)
 
     # context = {'giorno': stringa, 'erogA': eA, 'erogU': eU}
     # return render(request, 'chartInside.html', context)
 
-    # se voglio un grafico in cui sull'asse x ho tutti i giorni del periodo devo poter sapere quali sono questi giorni e contare per ognuno di loro i dati come
-    # se fossero un giorno singolo
-
-    # datePeriodo = DatiRaccolti.objects.values('date').distinct().filter(date__range=[stringaI, stringaF])  # primo compreso, ultimo escluso
-    # print(datePeriodo)
-
-    # return HttpResponse("quante date ci sono nel periodo ?")
+# views per grafico erogazioni giorno/notte
 
 
-def grafico_periodo(request):
-    # mettere context
-    return render(request, 'periodo.html')
+@csrf_exempt
+def invioGiornoNotte(request):
+    anno = request.GET.__getitem__('anno')
+    mese = request.GET.__getitem__('mese')
+    giorno = request.GET.__getitem__('giorno')
+    stringa = anno + "-" + mese + "-" + giorno
+    print(stringa)
 
+    eG = DatiRaccolti.objects.values('date').filter(date__gte=stringa, erogation=True, timeMod=True).order_by('date').count()  # giorno
+    eN = DatiRaccolti.objects.values('date').filter(date__gte=stringa, erogation=True, timeMod=False).order_by('date').count()  # notte
 
+    # dovro' mettere tipo date__day = giorno, date__month = mese , ...
+
+    print("eG: " + str(eG))
+    print("eN: " + str(eN))
+
+    erogazioni = [stringa, eG, eN]
+    return JsonResponse(erogazioni, safe=False)
+
+    # context = {'giorno': stringa, 'erogA': eA, 'erogU': eU}
+    # return render(request, 'chartInside.html', context)
 
